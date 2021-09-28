577081878c55dd5cf9b6e814fb86c2c8971657bb
bharath <bharath@email.com>
2021-09-28 09:08:28 +0530

    Added basic styling

--- SeedFakeCampgrounds/seedFakeCampgrounds.js
+++ SeedFakeCampgrounds/seedFakeCampgrounds.js
@@ -20,13 +20,21 @@ function getRandomCampgroundName(){
 }
 
 async function SaveRandomCampground() {
+    const campground = CreateRandomCampground()
+    await campground.save()
+}
+
+function CreateRandomCampground() {
     const randomCity = getRandomItemFromArray(cities)
+    const price = Math.floor((Math.random() * 10) + 10)
     const campground = new Campground({
         title: getRandomCampgroundName(),
         location: `${randomCity.city}, ${randomCity.state}`,
-        image: "https://source.unsplash.com/600x400/?hiking+trail"
+        image: "https://source.unsplash.com/300x300/?hiking+trail",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        price: price
     })
-    await campground.save()
+    return campground
 }
 
 const seedData = async function(){
@@ -41,5 +49,3 @@ const seedData = async function(){
 }
 
 seedData().then(()=> mongoose.connection.close())
-
-
--- models/Campgrounds.js
+++ models/Campgrounds.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const campgroundSchema = new Schema({
     title: String,
-    price: String,
+    price: Number,
     description: String,
     location: String,
     image: String
